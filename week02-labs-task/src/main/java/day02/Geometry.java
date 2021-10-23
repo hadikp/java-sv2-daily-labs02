9bd4f20b38a5c3130b21962b25ff74156855ed66
hadikp <hp20001025@gmail.com>
2021-10-23 21:28:43 +0200

    day02 konzultáció feladat

---
+++ week02-labs-task/src/main/java/day02/Geometry.java
@@ -0,0 +1,8 @@
+package day02;
+
+public class Geometry {
+    public static void main(String[] args) {
+        Rectangle rect = new Rectangle(3, 4);
+        System.out.println("A téglalap területe: " + rect.calculateArea());
+    }
+}
---
+++ week02-labs-task/src/main/java/day02/Rectangle.java
@@ -0,0 +1,15 @@
+package day02;
+
+public class Rectangle {
+    private double a;
+    private double b;
+
+    public Rectangle(double a, double b) {
+        this.a = a;
+        this.b = b;
+    }
+
+    public double calculateArea() {
+        return a * b;
+    }
+}
